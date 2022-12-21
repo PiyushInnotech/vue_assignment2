ad2383b865f3d9eca64e10a1d41054575e5940ae
unknown <piyushmangla@innotechteam.in>
2022-12-21 18:51:29 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+
+<template>
+  <div class="assign">
+    <div class="focusBtn">
+      <input type="text" ref="inputRef" />
+      <button @click="mounted">Focus</button>
+      <button @click="this.showModel=true">Show Modal</button>
+    </div>
+      <Modal
+        title="Signup for the Giveaway"
+        content="Grap your item for half prize"
+        theme="sales"
+        class="modal"
+        v-show="showModel"
+        @close="this.showModel=false"
+        @open="this.showModel=true"
+      >
+      </Modal>
+  </div>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      showModel: false
+    }
+  },
+  methods: {
+    mounted() {
+      this.$refs.inputRef.focus();
+    },
+  },
+  components: {
+    Modal,
+  },
+};
+</script>
+
+
+<style>
+.assign {
+  position: relative;
+  width: 100%;
+}
+.focusBtn {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.focusBtn input {
+  width: 200px;
+  height: 25px;
+}
+.focusBtn button {
+  width: 150px;
+  height: 30px;
+  font-size: 20px;
+  margin-top: 10px;
+}
+.modal {
+  position: absolute;
+  top: 0;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="box" @click="$emit('close')" >
+    <div :class="theme==='sale' ? 'red' : 'box1'" 
+          @click.prevent="$emit('open')">
+        <h1 class="boxTitle">{{title}}</h1>
+        <h3 class="boxContent">{{content}}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  data() {
+    return {
+      title:"",
+      content:"",
+      theme:'',
+    };
+  },
+  props:['title','content','theme', ],
+  emits:['close','open'],
+  methods: {
+  },
+};
+</script>
+
+<style>
+#Modal{
+    width: 100%;
+    height: 100vh;
+}
+.box{
+    width: 100%;
+    height: 100vh;
+    background-color: grey;
+    position: relative;
+    margin: 0;
+}
+.red{
+     width: 500px;
+    height: 250px;
+    border-radius: 25px;
+    position: absolute;
+    background-color: red;
+    top: 20%;
+    text-align: center;
+    color: white;
+    left: 35%;
+}
+.box1{
+    width: 500px;
+    height: 250px;
+    border-radius: 25px;
+    position: absolute;
+    background-color: white;
+    top: 20%;
+    text-align: center;
+    color: green;
+    left: 35%;
+}
+.boxTitle{
+    margin-top: 10%;
+    font-size: 40px;
+}
+.boxContent{
+    margin-top: 10%;
+    font-size: 25px;
+}
+
+</style>
